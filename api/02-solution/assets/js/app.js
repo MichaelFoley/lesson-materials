344b7334e075dfa2d650a2324f716d6728a3485b
Kathleen Tully <kathleen.tully@gmail.com>
2017-08-09 21:19:18 -0400

    Adding the solution we came up with together for comparison and reference. :D

--- api/02-solution/assets/js/app.js
+++ api/02-solution/assets/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     console.log('Page has loaded');
-    var corsMask = "https://cors.now.sh/";
     var placeholderImage = 'http://via.placeholder.com/210x295';
     var apiUrl = "http://api.tvmaze.com/search/shows?q=";
     var searchInput = '';
